c05c1f2cbc157c26e3995fa895a7ec6511d8f82d
Rushikesh Majale <rushimajale5453@gmail.com>
2024-03-28 07:32:06 +0530

    java

---
+++ java/Test.java
@@ -0,0 +1,13 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        float a = 1.11f;
+        System.out.println(a);
+    }
+}
